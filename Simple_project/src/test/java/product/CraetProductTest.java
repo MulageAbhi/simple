5277a96fe9b4f139705ee553bc9130fc20112943
Admin <Admin@DESKTOP-LN74GU8>
2022-01-26 16:45:20 +0530

    second commit

---
+++ Simple_project/src/test/java/product/CraetProductTest.java
@@ -0,0 +1,11 @@
+package product;
+
+import org.testng.annotations.Test;
+
+public class CraetProductTest {
+	@Test
+	public void createProductTest() {
+		System.out.println("crating product");
+	}
+
+}
